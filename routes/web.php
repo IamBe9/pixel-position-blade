de14109c4f714cb80cf65b87c02112d58f92c201
Daniel <u2_danhru_waw@technischoosl>
2025-04-27 22:07:02 +0200

    Routes for log & reg

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\JobController::class, 'index']);
 
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+
+Route::delete('/logout', [SessionController::class, 'destroy']);
+
+
+
