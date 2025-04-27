a87a777b4787406961e6c6ac9c4690dc71c7159a
Daniel <u2_danhru_waw@technischoosl>
2025-04-27 22:49:54 +0200

    controller for seach + completed search

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\JobController::class, 'index']);
+Route::get('/search', SearchController::class);
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
